010831d9049416c4145aacaf9fd3bbf5065c746f
diogo-SG <diogo.carneiro@studiographene.com>
2026-01-22 12:33:43 +0000

    Implement favorites functionality in FavouritesScreen
    - Integrated Redux to fetch favorite meals from the state.
    - Updated FavouritesScreen to display a FlatList of favorite meals or a message when no favorites are found.
    - Enhanced user experience by providing immediate feedback on the favorites list.

--- screens/FavouritesScreen.tsx
+++ screens/FavouritesScreen.tsx
@@ -1,10 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import MealItem from "@/components/MealItem";
+import { MEALS } from "@/dummy-data";
+import Meal from "@/models/meal";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
 
 export default function FavouritesScreen() {
    
+  const favourites = useSelector((state: any) => state.favourites.ids);
+  const meals = MEALS.filter((meal: Meal) => favourites.includes(meal.id));
+
+  if (meals.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text>No favourites found</Text>
+      </View>
+    );
+  }
+  
     return (
     <View style={styles.container}>
-      <Text>Favourites</Text>
+      <FlatList data={meals} renderItem={({ item }) => <MealItem meal={item as Meal} />} keyExtractor={(item) => item.id} />
     </View>
   );
 }   
